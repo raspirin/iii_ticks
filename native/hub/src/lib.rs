586bed665842144e3e337b768820faded731ad7f
Raspirin <f1193968991@126.com>
2024-04-05 22:14:02 +0800

    feat: main.proto

--- native/hub/src/lib.rs
+++ native/hub/src/lib.rs
@@ -1,21 +1,21 @@
-//! This `hub` crate is the
-//! entry point of the Rust logic.
-
-// This `tokio` will be used by Rinf.
-// You can replace it with the original `tokio`
-// if you're not targeting the web.
+use rinf::debug_print;
 use tokio_with_wasm::tokio;
 
 mod messages;
 
 rinf::write_interface!();
 
-// Always use non-blocking async functions
-// such as `tokio::fs::File::open`.
-// If you really need to use blocking code,
-// use `tokio::task::spawn_blocking`.
 async fn main() {
-    // Repeat `tokio::spawn` anywhere in your code
-    // if more concurrent tasks are needed.
-    // tokio::spawn(sample_functions::tell_numbers());
+    tokio::spawn(start());
+}
+
+pub async fn start() {
+    use messages::main::*;
+
+    let mut receiver = StartButtonPressed::get_dart_signal_receiver();
+    while let Some(msg) = receiver.recv().await {
+        let _start_button_pressed = msg.message;
+
+        debug_print!("hello, world");
+    } 
 }
