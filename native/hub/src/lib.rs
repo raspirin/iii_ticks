5c66ae22a4cd5bdac0c291fe0232540f7da59c44
Raspirin <f1193968991@126.com>
2024-04-09 18:02:29 +0800

    feat: click the button and play

--- native/hub/src/lib.rs
+++ native/hub/src/lib.rs
@@ -1,27 +1,31 @@
 #![deny(clippy::unwrap_used)]
 
-use rinf::debug_print;
-use tokio_with_wasm::tokio;
+use player_thread::init_player_thread;
+use tokio_with_wasm::tokio::{self};
 
 mod audio;
 #[allow(clippy::unwrap_used)]
 mod messages;
 mod output;
 mod player;
+mod player_thread;
 
 rinf::write_interface!();
 
 async fn main() {
-    tokio::spawn(start());
+    tokio::spawn(start(init_player_thread()));
 }
 
-pub async fn start() {
+pub async fn start(sender: std::sync::mpsc::Sender<player_thread::ThreadMessage>) {
     use messages::main::*;
 
-    let mut receiver = StartButtonPressed::get_dart_signal_receiver();
-    while let Some(msg) = receiver.recv().await {
-        let _start_button_pressed = msg.message;
-
-        debug_print!("hello, world");
+    let mut dart_signal_receiver = StartButtonPressed::get_dart_signal_receiver();
+    while let Some(_) = dart_signal_receiver.recv().await {
+        sender
+            .send(player_thread::ThreadMessage::Play(
+                "assets/native/test.ogg".into(),
+            ))
+            // TODO: fix this unwrap
+            .unwrap();
     }
 }
---
+++ native/hub/src/player_thread.rs
@@ -0,0 +1,57 @@
+use std::sync::mpsc::{Receiver, TryRecvError};
+
+use rinf::debug_print;
+
+use crate::player::{Player, PlayerError};
+
+pub enum ThreadMessage {
+    Play(String),
+    Pause,
+}
+
+/// Init a sync thread which listen to tokio thread and holds the player.
+pub fn init_player_thread() -> std::sync::mpsc::Sender<ThreadMessage> {
+    let (tx, rx) = std::sync::mpsc::channel::<ThreadMessage>();
+    let player_thread_handle = std::thread::spawn(move || {
+        thread(rx, |e| {
+            debug_print!("{e}");
+        })
+    });
+    tx
+}
+
+pub fn thread(rx: Receiver<ThreadMessage>, error_fn: impl FnOnce(PlayerError) + std::marker::Copy) {
+    let mut player: Option<Player> = None;
+    let receiver = rx;
+    loop {
+        match receiver.try_recv() {
+            Ok(ThreadMessage::Pause) => {
+                player.as_mut().map(|x| x.pause());
+            }
+            Ok(ThreadMessage::Play(path)) => {
+                if player.is_none() {
+                    player = match Player::new(&path) {
+                        Ok(p) => Some(p),
+                        Err(e) => {
+                            error_fn(e);
+                            None
+                        }
+                    }
+                } else {
+                    player.as_mut().map(|x| {
+                        if let Err(e) = x.open(&path) {
+                            error_fn(e)
+                        }
+                    });
+                }
+            }
+            Err(TryRecvError::Empty) => {
+                player.as_mut().map(|x| x.tick());
+            }
+            Err(TryRecvError::Disconnected) => {
+                debug_print!("rust thread disconnected, quit player thread.");
+                break;
+            }
+        }
+    }
+}
