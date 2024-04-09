fd20b58b3507c291e1fa6c0e15f11bf9c0943d14
Raspirin <f1193968991@126.com>
2024-04-09 16:28:46 +0800

    feat: switch to anothor stream

--- native/hub/src/player.rs
+++ native/hub/src/player.rs
@@ -1,10 +1,9 @@
 use thiserror::Error;
-
 use crate::{
     audio::{Audio, AudioError},
     output::{self, AudioOutput, AudioOutputError},
 };
-use std::{fs::File, io};
+use std::{collections::HashMap, fs::File, io};
 
 #[derive(Error, Debug)]
 pub enum PlayerError {
@@ -23,23 +22,49 @@ pub enum PlayerError {
 }
 
 pub struct Player {
-    audio: Audio,
+    audio_map: HashMap<String, Audio>,
     output: Option<Box<dyn AudioOutput>>,
+    audio: String,
 }
 
 impl Player {
     pub fn new(path: &str) -> Result<Self, PlayerError> {
         let file = File::open(path)?;
         let audio = Audio::try_new(Box::new(file))?;
+        let key = path.to_string();
+        let mut map = HashMap::new();
+        map.insert(key, audio);
 
         Ok(Self {
-            audio,
+            audio_map: map,
             output: None,
+            audio: path.to_string(),
         })
+    }
+    
+    pub fn open(&mut self, path: &str) -> Result<(), PlayerError> {
+        if !self.audio_map.contains_key(path) {
+            let file = File::open(path)?;
+            let audio = Audio::try_new(Box::new(file))?;
+            self.audio_map.insert(path.to_string(), audio);
+        }
+
+        self.audio = path.to_string();
+        self.output.as_mut().map(|x| x.flush());
+
+        Ok(())
+    }
+
+    pub fn get_audio(&self) -> &Audio {
+        &self.audio_map[&self.audio]
+    }
+
+    pub fn get_aduio_mut(&mut self) -> &mut Audio {
+        self.audio_map.get_mut(&self.audio).unwrap()
     }
 
     pub fn tick(&mut self) -> Result<(), PlayerError> {
-        let packet = match self.audio.format.next_packet() {
+        let packet = match self.get_aduio_mut().format.next_packet() {
             Ok(p) => p,
             Err(symphonia::core::errors::Error::ResetRequired) => unimplemented!(),
             Err(symphonia::core::errors::Error::IoError(e)) => {
@@ -53,11 +78,11 @@ impl Player {
             }
             Err(e) => return Err(PlayerError::PacketError(e)),
         };
-        if packet.track_id() != self.audio.track_id {
+        if packet.track_id() != self.get_audio().track_id {
             return Ok(());
         }
 
-        match self.audio.decoder.decode(&packet) {
+        match self.audio_map.get_mut(&self.audio).unwrap().decoder.decode(&packet) {
             Ok(decoded) => {
                 if self.output.is_none() {
                     let spec = *decoded.spec();
