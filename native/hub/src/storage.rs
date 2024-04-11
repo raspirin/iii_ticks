bfb2c0968dddd5f0b5350fdce037696830af5cbd
Raspirin <f1193968991@126.com>
2024-04-11 14:37:09 +0800

    feat: make sure the folder exists

--- native/hub/src/storage.rs
+++ native/hub/src/storage.rs
@@ -1,9 +1,9 @@
-use std::path::PathBuf;
+use std::{path::PathBuf};
 
 use directories::BaseDirs;
 use lazy_static::lazy_static;
 use rinf::debug_print;
-use tokio_with_wasm::tokio::sync::Mutex;
+use tokio_with_wasm::tokio::{fs, sync::Mutex};
 
 use crate::messages::main::PlatformPathMessage;
 
@@ -17,8 +17,8 @@ pub async fn init_persist_storage() {
     use crate::messages::*;
 
     let mut rx = PlatformPathMessage::get_dart_signal_receiver();
+    let mut config_path = CONFIG_PATH.lock().await;
     while let Some(msg) = rx.recv().await {
-        let mut config_path = CONFIG_PATH.lock().await;
         if config_path.is_none() {
             let path = PathBuf::from(msg.message.config_path);
             config_path.replace(path);
@@ -26,5 +26,9 @@ pub async fn init_persist_storage() {
         break;
     }
 
-    
+    config_path.as_ref().map(|path| async move {
+        if !path.is_dir() {
+            fs::create_dir(path).await.unwrap();
+        }
+    });
 }
